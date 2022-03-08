1f3b95afbf94c9c0bf3556cc98eec51a0ac3e2e7
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-03-07 23:29:22 -0300

    V 1.0

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templetes/views')
+const partialsPath = path.join(__dirname, '../templetes/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+ 
+    })
+})  
+   
+         
+       
+        
+       
+       
+
+
+app.listen(port, () => {
+    console.log('Server is online on port ' + port)
+})
